ce9c23467de45ffb017329a2a340ff02d2c95d6c
Daniel <danieldoreino@gmail.com>
2025-03-27 11:32:37 -0300

    ajustes para o deploy

--- back-end/api/server.js
+++ back-end/api/server.js
@@ -1,6 +1,10 @@
 import express from "express";
 import { db } from "./connect.js";
 import cors from "cors";
+import path, { dirname } from "path";
+
+const __dirname = path.resolve();
+console.log(__dirname);
 
 const app = express();
 const PORT = 3000;
@@ -8,16 +12,22 @@ const PORT = 3000;
 app.use(cors());
 // app.use(express.json());
 
-app.get("/", (request, response) => {
+app.get("/api/", (request, response) => {
   response.send("Olá, Mundo!");
 });
 
-app.get("/artists", async (request, response) => {
+app.get("/api/artists", async (request, response) => {
   response.send(await db.collection("artists").find({}).toArray());
 });
 
-app.get("/songs", async (request, response) => {
+app.get("/api/songs", async (request, response) => {
   response.send(await db.collection("songs").find({}).toArray());
+});
+
+app.use(express.static(path.join(__dirname, "../front-end/dist")));
+
+app.get("*", async (request, response) => {
+  response.sendFile(path.join(__dirname, "../front-end/dist/index.html"));
 });
 
 app.listen(PORT, () => {
